0967786797a4da2158ec18e85cd829616483f070
jmackereth <tedmackereth@gmail.com>
2019-10-31 13:45:32 +0000

    include measure_apogee

--- sewingmachine/measure_apogee.py
+++ sewingmachine/measure_apogee.py
@@ -14,13 +14,11 @@ def measure_apogee(allStar, linelist_obj, output_fits=False, *args, **kwargs):
     errs = np.empty([np.shape(allStar)[0], np.shape(linelist_obj.labels)[0]])
     for i in tqdm(range(len(apogee_ids))):
         try:
-            specs, hdr = apread.aspcapStar(allStar['LOCATION_ID'][i], allStar['APOGEE_ID'][i], ext=1)
-            errspec, hdr = apread.aspcapStar(allStar['LOCATION_ID'][i], allStar['APOGEE_ID'][i], ext=2)
+            specs, hdr = apread.aspcapStar(str(allStar['LOCATION_ID'][i]), allStar['APOGEE_ID'][i], ext=1)
+            errspec, hdr = apread.aspcapStar(str(allStar['LOCATION_ID'][i]), allStar['APOGEE_ID'][i], ext=2)
             spec = np.dstack([lams, specs, errspec])[0]
             out = equivalentwidths.measurelinelist(spec, linelist_obj, error=True, *args, **kwargs)
             ews[i], errs[i] = out[0], out[1]
-            if kwargs.get('return_flags', False):
-
         except IOError:
             print('Spectrum missing from SAS?')
             ews[i], errs[i] = np.ones(np.shape(linelist_obj.labels)[0])*np.nan, np.ones(np.shape(linelist_obj.labels)[0])*np.nan
